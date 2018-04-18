0db6ad9b6f30e57526442e6bba50a158edee7080
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-04-18 13:48:25 +0100

    added endpoint

--- src/main/java/uk/gov/homeoffice/borders/workflow/resource/EngineResourceLoadingConfiguration.java
+++ src/main/java/uk/gov/homeoffice/borders/workflow/resource/EngineResourceLoadingConfiguration.java
@@ -2,14 +2,17 @@ package uk.gov.homeoffice.borders.workflow.resource;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Slf4j
 public class EngineResourceLoadingConfiguration {
 
 
@@ -23,14 +26,29 @@ public class EngineResourceLoadingConfiguration {
         private String accessKey;
         @Value("${engine.resource.s3.secretKey}")
         private String secretKey;
-
+        @Value("${engine.resource.s3.endpoint:default}")
+        private String s3Endpoint;
+        @Value("${engine.resource.s3.region:eu-west-2}")
+        private String region;
 
         @Bean
         public AmazonS3 amazonS3Client() {
             BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-            return AmazonS3ClientBuilder.standard()
-                    .withRegion("eu-west-2")
-                    .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+            if (s3Endpoint.equalsIgnoreCase("default")) {
+                log.info("Using default configuration....");
+                return AmazonS3ClientBuilder.standard()
+                        .withRegion(region)
+                        .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+
+            } else {
+                log.info("Using S3 endpoint configuration....");
+                return AmazonS3ClientBuilder.standard()
+                        .withEndpointConfiguration(
+                                new AwsClientBuilder.EndpointConfiguration(s3Endpoint, region)
+                        ).withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+
+            }
+
         }
 
 
