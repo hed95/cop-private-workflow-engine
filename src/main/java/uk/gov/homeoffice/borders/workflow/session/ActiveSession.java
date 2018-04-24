706e13139a6bf637f16b1319255202abce137a3f
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-04-24 14:33:44 +0100

    updated model

--- src/main/java/uk/gov/homeoffice/borders/workflow/session/ActiveSession.java
+++ src/main/java/uk/gov/homeoffice/borders/workflow/session/ActiveSession.java
@@ -1,5 +1,6 @@
 package uk.gov.homeoffice.borders.workflow.session;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
