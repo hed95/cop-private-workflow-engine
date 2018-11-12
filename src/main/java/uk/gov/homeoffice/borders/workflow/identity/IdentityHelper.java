4db08af8ab90d5767f5a4d7e9d52d1b132d0c12c
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-11-12 14:25:14 +0000

    updating helper

--- src/main/java/uk/gov/homeoffice/borders/workflow/identity/IdentityHelper.java
+++ src/main/java/uk/gov/homeoffice/borders/workflow/identity/IdentityHelper.java
@@ -2,7 +2,6 @@ package uk.gov.homeoffice.borders.workflow.identity;
 
 
 import org.camunda.bpm.engine.IdentityService;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.homeoffice.borders.workflow.security.WorkflowAuthentication;
@@ -18,18 +17,12 @@ public class IdentityHelper {
     @Autowired
     private IdentityService identityService;
 
-    /**
-     * Sets a process variable 'candidateGroupsForCurrentUser'. You can use this in the following:
-     * <p>#{identityHelper.setCandidateGroupsForCurrentUser(execution)}</p>
-     * @param delegateExecution
-     */
-    public void setCandidateGroupsForCurrentUser(DelegateExecution delegateExecution) {
+    public String candidateGroupsForCurrentUser() {
         WorkflowAuthentication currentAuthentication = (WorkflowAuthentication) identityService.getCurrentAuthentication();
-        String candidateGroups =  ofNullable(currentAuthentication).map((authentication) -> {
+
+        return ofNullable(currentAuthentication).map((authentication) -> {
             List<String> teamCodes = authentication.getUser().getTeams().stream().map(Team::getTeamCode).collect(toList());
             return String.join(",", teamCodes);
         }).orElse("");
-
-        delegateExecution.setVariable("candidateGroupsForCurrentUser", candidateGroups);
     }
 }
