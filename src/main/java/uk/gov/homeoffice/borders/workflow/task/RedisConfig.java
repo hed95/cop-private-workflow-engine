91ad1ddf75992d1e2fe2de5150d6644ea461a145
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-11-21 10:29:25 +0000

    reverting ssl comment

--- src/main/java/uk/gov/homeoffice/borders/workflow/task/RedisConfig.java
+++ src/main/java/uk/gov/homeoffice/borders/workflow/task/RedisConfig.java
@@ -47,8 +47,8 @@ public class RedisConfig {
                 .builder()
                 .usePooling()
                 .poolConfig(poolConfig)
-//                .and()
-//                .useSsl()
+                .and()
+                .useSsl()
                 .build();
 
 
