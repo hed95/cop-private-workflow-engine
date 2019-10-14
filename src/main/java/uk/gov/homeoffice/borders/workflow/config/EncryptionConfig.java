355ef08282105c398f865fe6c4f462238d48d1a4
Amin <amin@digitalpatterns.io>
2019-10-14 14:31:19 +0100

    WIP: Process variable encryption

---
+++ src/main/java/uk/gov/homeoffice/borders/workflow/config/EncryptionConfig.java
@@ -0,0 +1,36 @@
+package uk.gov.homeoffice.borders.workflow.config;
+
+import io.digitalpatterns.camunda.encryption.ProcessInstanceSpinVariableDecryptor;
+import io.digitalpatterns.camunda.encryption.ProcessInstanceSpinVariableEncryptionPlugin;
+import io.digitalpatterns.camunda.encryption.ProcessInstanceSpinVariableEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!test")
+public class EncryptionConfig {
+
+    @Value("${encryption.keys.private-path}")
+    private String pathToPrivateKey;
+
+    @Value("${encryption.keys.public-path}")
+    private String pathToPublicKey;
+
+    @Bean
+    public ProcessInstanceSpinVariableDecryptor processInstanceSpinVariableDecryptor() {
+        return new ProcessInstanceSpinVariableDecryptor(pathToPrivateKey);
+    }
+
+    @Bean
+    public ProcessInstanceSpinVariableEncryptor processInstanceSpinVariableEncryptor() {
+        return new ProcessInstanceSpinVariableEncryptor(pathToPrivateKey);
+    }
+
+
+    @Bean
+    public ProcessInstanceSpinVariableEncryptionPlugin plugin() {
+        return new ProcessInstanceSpinVariableEncryptionPlugin(processInstanceSpinVariableEncryptor(), processInstanceSpinVariableDecryptor());
+    }
+}
